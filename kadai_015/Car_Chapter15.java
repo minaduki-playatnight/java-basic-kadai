5e35b37c0d80711a04067dedec95baa57700682b
minaduki-playatnight <minase0402zzz@gmail.com>
2025-11-16 22:05:29 +0900

    java課題15

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,32 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+    private int gear;
+    private int speed;
+
+    public Car_Chapter15(int gear, int speed) {
+        this.gear = gear;
+        this.speed = speed;
+    }
+
+    public void changeGear(int afterGear) {
+        int beforeGear = this.gear;
+        this.gear = afterGear;
+
+        this.speed = switch (afterGear) {
+            case 1 -> 10;
+            case 2 -> 20;
+            case 3 -> 30;
+            case 4 -> 40;
+            case 5 -> 50;
+            default -> 10;
+        };
+
+        System.out.println("ギア" + beforeGear + "から" + afterGear + "に切り替えました");
+    }
+
+    public void run() {
+        System.out.println("速度は時速" + this.speed + "kmです");
+    }
+}
