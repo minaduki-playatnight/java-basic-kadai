0f74a87190e486950db9b40b6044f3af4232465c
minaduki-playatnight <minase0402zzz@gmail.com>
2025-11-30 01:15:34 +0900

    Update Dictionary_Chapter21.java
    課題再提出のため修正

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -4,19 +4,52 @@ import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
 
-	HashMap<String, String> fruitsMap = new HashMap<String,String>();
+	private HashMap<String, String> fruitsMap;
 
-	{
-        fruitsMap.put("apple", "りんご");
-        fruitsMap.put("peach", "桃");
-        fruitsMap.put("banana", "ばなな");
-        fruitsMap.put("lemon", "レモン");
-        fruitsMap.put("pear", "梨");
-        fruitsMap.put("kiwi", "キウィ");
-        fruitsMap.put("strawberry", "いちご");
-        fruitsMap.put("grape", "ぶどう");
-        fruitsMap.put("muscat", "マスカット");
-        fruitsMap.put("cherry", "さくらんぼ");
-    }
-    
+		public Dictionary_Chapter21() {
+	        fruitsMap = new HashMap<>();
+			fruitsMap.put("apple", "りんご");
+	        fruitsMap.put("peach", "桃");
+	        fruitsMap.put("banana", "バナナ");
+	        fruitsMap.put("lemon", "レモン");
+	        fruitsMap.put("pear", "梨");
+	        fruitsMap.put("kiwi", "キウィ");
+	        fruitsMap.put("strawberry", "いちご");
+	        fruitsMap.put("grape", "ぶどう");
+	        fruitsMap.put("muscat", "マスカット");
+	        fruitsMap.put("cherry", "さくらんぼ");			
+			}
+
+	
+
+	
+	public void dicFruits(String[] addFruits){
+		
+        if (addFruits == null) {
+            System.out.println("入力が null です");
+            return;
+        }		
+        
+		for( int i = 0; i < addFruits.length; i++){
+			
+			String key = addFruits[i];
+			String value = fruitsMap.get(key);
+		
+			if(value != null) {
+			
+				System.out.println(key+ "の意味は"+ value);
+			
+			}else {
+				System.out.println(key + "は辞書に存在しません");
+			}
+		}		
+	}
 }
+
+
+	
+	
+	
+	
+    
+
