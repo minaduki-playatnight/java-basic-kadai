a2fba73dfe4051e9531721c6a7b46abffa49d38f
minaduki-playatnight <minase0402zzz@gmail.com>
2025-11-30 01:15:10 +0900

    Update DictionaryExec_Chapter21.java
    課題再提出のため訂正

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -9,18 +9,9 @@ public class DictionaryExec_Chapter21 {
 		
 		String[] fruitsName = {"apple","banana","grape","orange"};
 		
-		
-		for( int i = 0; i < fruitsName.length; i++){
-			
-			if(dic.fruitsMap.get(fruitsName[i]) != null) {
-				
-				System.out.println(fruitsName[i] + "の意味は"+ dic.fruitsMap.get(fruitsName[i]));
-				
-			}else {
-				System.out.println(fruitsName[i] + "は辞書に存在しません");
-				
-			}		
-		}	
-	}
 
+		dic.dicFruits(fruitsName);
+		
+	}
+	
 }
