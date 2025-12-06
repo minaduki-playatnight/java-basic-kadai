564af162499d96e5fbf00495681c15047efc2779
minaduki-playatnight <minase0402zzz@gmail.com>
2025-12-06 12:51:41 +0900

    java課題028

---
+++ kadai_028/JyankenExec_Chapter28.java
@@ -0,0 +1,29 @@
+package kadai_028;
+
+public class JyankenExec_Chapter28 {
+
+	public static void main(String[] args) {
+
+		
+		//Jyanken_Chapter28で記載したメソッドを順次呼び出す
+
+		Jyanken_Chapter28 janken = new Jyanken_Chapter28();
+		
+		janken.playGame();
+		
+		
+		
+		//以下だとうまく動かなかった
+		//手を入力してもらう
+		//janken.getMyChoice();
+		
+		//PCの手の決定
+		//janken.getRandom() ;
+		
+		//勝敗の決定
+		//janken.playGame();
+		
+		
+	}
+
+}
